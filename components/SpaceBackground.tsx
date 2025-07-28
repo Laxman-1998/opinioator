bfd82d68701f5492ad9a2fb1ca9282c5d24bcb3c
Laxman-1998 <varadhilaxman@gmail.com>
2025-07-28 23:19:44 +0000

    Get done 5

---
+++ components/SpaceBackground.tsx
@@ -0,0 +1,91 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import type { Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
+
+const SpaceBackground = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full -z-10">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          background: {
+            color: {
+              value: "#0B1120",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onHover: {
+                enable: true,
+                mode: "bubble",
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 200,
+                duration: 2,
+                opacity: 0.8,
+                size: 2,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: false,
+              opacity: 0.1,
+              width: 1,
+            },
+            collisions: {
+              enable: false,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "out",
+              },
+              random: true,
+              speed: 0.1,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 100,
+            },
+            opacity: {
+              value: { min: 0.1, max: 0.5},
+               animation: {
+                enable: true,
+                speed: 1,
+                minimumValue: 0.1,
+              }
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 2 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </div>
+  );
+};
+export default SpaceBackground;
