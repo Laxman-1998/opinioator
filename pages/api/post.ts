d39a09beb61f3054fe767d66a885094ad7a826ff
Laxman-1998 <varadhilaxman@gmail.com>
2025-08-01 22:36:53 +0000

    anonymous_name

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { supabase } from "@/lib/supabaseClient";
-import { generateAnonymousName } from "@/lib/generateAnonymousName";
+import { supabase } from "../../lib/supabaseClient";
+import { generateAnonymousName } from "../../lib/generateAnonymousName";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
