53eb848f34b9b9c5ab5e1a75244ba1fc4b693135
Laxman-1998 <varadhilaxman@gmail.com>
2025-08-07 22:15:06 +0000

    build: Final architecture synchronization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
-import 'rc-slider/assets/index.css';
 import '../styles/globals.css';
+import 'rc-slider/assets/index.css';
 import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
 import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from '../lib/auth'; // Import our new AuthProvider
+import { AuthProvider } from '../lib/auth';
+import { ModalProvider } from '../context/ModalContext';
+import Header from '../components/Header'; // We will include the Header directly
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // We wrap the entire app with the AuthProvider
     <AuthProvider>
-      <Layout>
+      <ModalProvider>
+        <div className="min-h-screen bg-slate-950 text-white">
+          <Header /> {/* The Header is now part of the main app structure */}
+          <main className="container mx-auto p-4 md:p-8">
+            <Component {...pageProps} />
+          </main>
+        </div>
         <Toaster
           position="bottom-center"
           toastOptions={{
@@ -19,8 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             },
           }}
         />
-        <Component {...pageProps} />
-      </Layout>
+      </ModalProvider>
     </AuthProvider>
   );
 }
