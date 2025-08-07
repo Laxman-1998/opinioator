6cf93d88b3c85f30e68be01bf77499c71e293396
Laxman-1998 <varadhilaxman@gmail.com>
2025-08-07 22:20:28 +0000

    style: Restore original content width

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,16 @@ import type { AppProps } from 'next/app';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from '../lib/auth';
 import { ModalProvider } from '../context/ModalContext';
-import Header from '../components/Header'; // We will include the Header directly
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <ModalProvider>
         <div className="min-h-screen bg-slate-950 text-white">
-          <Header /> {/* The Header is now part of the main app structure */}
-          <main className="container mx-auto p-4 md:p-8">
+          <Header />
+          {/* 👇 THE FIX IS HERE: We are using a more constrained max-width */}
+          <main className="max-w-5xl mx-auto p-4 md:p-8">
             <Component {...pageProps} />
           </main>
         </div>
